8f6370bdd86b97c13189734fb98bbe617231b732
Mujeeb <adejobimujeeb5@gmail.com>
2021-01-26 12:57:57 +0100

    initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Sidebar.css";
+
+function Sidebar() {
+    return (
+        <div className="sideBar">
+            
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAV_k0r1NZ3mWXUonxypN3eALhFc2zX420",
+    authDomain: "spotify-clone-6a51c.firebaseapp.com",
+    databaseURL: "https://spotify-clone-6a51c.firebaseio.com",
+    projectId: "spotify-clone-6a51c",
+    storageBucket: "spotify-clone-6a51c.appspot.com",
+    messagingSenderId: "442921439758",
+    appId: "1:442921439758:web:dd68c0383011fa7a6ca25f",
+    measurementId: "G-D720GKK9E6"
+  };
